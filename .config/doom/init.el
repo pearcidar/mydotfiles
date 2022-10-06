2f218673e1e0d6d2adcaad3d78d3cfc0267e3c2d
tanbinislam43 <islamtanbin43@gmail.com>
2022-10-06 07:17:23 +0000

    system borked :p

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -186,9 +186,9 @@
        emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
-       ;;(rss +org)        ; emacs as an RSS reader
+       (rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       ;literate
        (default +bindings +smartparens))
